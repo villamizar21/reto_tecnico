81c8b26db0b50c7b8554ba24e9938af64269b265
villamizar21 <arleyvillamizar@wposs.com>
2022-03-25 15:51:00 -0500

    Se agrega el modelo de la base de datos

---
+++ reto_tecnico/src/modelo/Pregunta.java
@@ -0,0 +1,112 @@
+package modelo;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pregunta")
+@NamedQueries({
+    @NamedQuery(name = "Pregunta.findAll", query = "SELECT p FROM Pregunta p")
+    , @NamedQuery(name = "Pregunta.findById", query = "SELECT p FROM Pregunta p WHERE p.id = :id")})
+public class Pregunta implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "Id", nullable = false)
+    private Integer id;
+    @Basic(optional = false)
+    @Lob
+    @Column(name = "Pregunta", nullable = false, length = 65535)
+    private String pregunta;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pregunta")
+    private List<Opcion> opcionList;
+    @JoinColumn(name = "Categoria", referencedColumnName = "Id", nullable = false)
+    @ManyToOne(optional = false)
+    private Categoria categoria;
+
+    public Pregunta() {
+    }
+
+    public Pregunta(Integer id) {
+        this.id = id;
+    }
+
+    public Pregunta(Integer id, String pregunta) {
+        this.id = id;
+        this.pregunta = pregunta;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPregunta() {
+        return pregunta;
+    }
+
+    public void setPregunta(String pregunta) {
+        this.pregunta = pregunta;
+    }
+
+    public List<Opcion> getOpcionList() {
+        return opcionList;
+    }
+
+    public void setOpcionList(List<Opcion> opcionList) {
+        this.opcionList = opcionList;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Pregunta)) {
+            return false;
+        }
+        Pregunta other = (Pregunta) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "modelo.Pregunta[ id=" + id + " ]";
+    }
+    
+}
